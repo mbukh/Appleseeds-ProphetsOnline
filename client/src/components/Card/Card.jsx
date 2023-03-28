2eecbccebb3440cb609b62ad4d90cd6d677a9bbb
amal khanjer <amlkh2724@gmail.com>
2023-03-28 20:16:44 +0300

    create some functions in client

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+import Post from '../Post/Post.jsx'
+const Card = () => {
+  return (
+    <div className="borderBox">
+
+    <Post></Post>
+
+    </div>
+  )
+}
+export default Card
