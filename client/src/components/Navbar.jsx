4d73b7c31a63aed56567758cc3c7e9b8366743d0
amal khanjer <amlkh2724@gmail.com>
2023-03-29 09:31:41 +0300

    added more func

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,13 +19,6 @@ const StyledToolbar = styled(Toolbar)({
   justifyContent: "space-between",
 });
 
-// const Search = styled("div")(({ theme }) => ({
-//   backgroundColor: "white",
-//   padding: "0 10px",
-//   borderRadius: theme.shape.borderRadius,
-//   width: "40%",
-// }));
-
 const Icons = styled(Box)(({ theme }) => ({
   display: "none",
   alignItems: "center",
@@ -49,17 +42,14 @@ const Navbar = () => {
     <AppBar position="sticky">
       <StyledToolbar>
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
-          LAMA DEV
+          added name
         </Typography>
         <Pets sx={{ display: { xs: "block", sm: "none" } }} />
         <Search>  
         
 
         </Search>
-        {/* <Search>
-          <InputBase placeholder="search..." />
 
-        </Search> */}
         <Icons>
           <Badge badgeContent={4} color="error">
             <Mail />
--- server/server.js
+++ server/server.js
@@ -1,35 +1,41 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
-dotenv.config({ path: './server/config/config.env' });
+const cors = require("cors");
+
+mongoose.set("strictQuery", false);
+dotenv.config({ path: "./server/config/config.env" });
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));//know body in sending requst
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true })); //know body in sending requst
 app.use(express.json());
 
-
 // app.use('/', (req, res) => res.send('Server running'));
-app.use('/Author', require("./Route/AuthorRoute"));
-app.use('/Post', require("./Route/PostRoute"));
-app.use('/Comment', require("./Route/CommentRoute"));
-
+app.use("/Author", require("./Route/AuthorRoute"));
+app.use("/Post", require("./Route/PostRoute"));
+app.use("/Comment", require("./Route/CommentRoute"));
 
 // app.use(cors({
 //   origin: 'http://127.0.0.1:5173',
 //   credentials: true
 // }));
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   const MONGO_URI = process.env.MONGO_URI;
   try {
     await mongoose.connect(MONGO_URI);
   } catch (error) {
-    throw new Error("failed to connect to db")
+    throw new Error("failed to connect to db");
   }
 
-  app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-}
+  app.listen(
+    PORT,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  );
+};
 start();
-
